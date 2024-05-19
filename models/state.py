11cadbef04e07c46cbd495e35fe521ac11fa89c1
Ngumi22 <mwaurapngumi@gmail.com>
2024-05-19 21:05:03 +0300

    Additions

--- models/state.py
+++ models/state.py
@@ -2,8 +2,30 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from os import getenv
+
+
+STORAGE_TYPE = getenv("HBNB_TYPE_STORAGE")
+
 
 class State(BaseModel, Base):
+    """Represents a state class"""
+
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref="state")
+
+    if STORAGE_TYPE != 'db':
+        name = ''
+        cities = []
+
+        @property
+        def cities(self):
+            """Gets a list of City instances with
+               state_id equal to current state.id
+            """
+            city_list = []
+            for city in models.storage.all(city):
+                if city.state.id == self.id:
+                    city_list.append(city)
+            return city_list
